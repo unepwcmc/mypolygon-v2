3f718d05d8d30ec26fb8b2995721d090dae1e02e
strtwtsn <stuart.watson@unep-wcmc.org>
2013-12-03 11:44:51 +0000

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,11 @@
 set :rails_env, "production"
 # Primary domain name of your application. Used in the Apache configs
-set :domain, "unepwcmc-013.vm.brightbox.net"
+set :domain, "unepwcmc-014.vm.brightbox.net"
 ## List of servers
-server "unepwcmc-013.vm.brightbox.net", :app, :web, :db, :primary => true
+server "unepwcmc-014.vm.brightbox.net", :app, :web, :db, :primary => true
 
 set :application, "mypolygonv2"
-set :server_name, "mypolygonv2.unepwcmc-013.vm.brightbox.net"
+set :server_name, "mypolygonv2.unepwcmc-014.vm.brightbox.net"
 set :sudo_user, "rails"
 set :app_port, "80" 
 
@@ -17,7 +17,7 @@ server {
   listen 80;
   
   client_max_body_size 4G;
-  server_name #{application}.unepwcmc-013.vm.brightbox.net #{application}.sw02.matx.info;
+  server_name #{application}.unepwcmc-014.vm.brightbox.net #{application}.sw02.matx.info;
   keepalive_timeout 5;
   root #{deploy_to}/current/public
   passenger_enabled on;
